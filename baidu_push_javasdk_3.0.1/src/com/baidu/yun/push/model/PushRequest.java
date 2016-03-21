10c8f4637659fdcf0d6547fdc2833ba97336cf23
codecly259 <maxinchun0215@qq.com>
2016-03-21 23:52:42 +0800

    PushRequest抽象类

--- baidu_push_javasdk_3.0.1/src/com/baidu/yun/push/model/PushRequest.java
+++ baidu_push_javasdk_3.0.1/src/com/baidu/yun/push/model/PushRequest.java
@@ -3,7 +3,13 @@ package com.baidu.yun.push.model;
 import com.baidu.yun.core.annotation.HttpParamKeyName;
 import com.baidu.yun.core.annotation.R;
 import com.baidu.yun.push.constants.BaiduPushConstants;
-
+/**
+ * 推送请求类
+ * 为抽象类，具体每个请求的请求类会继承此类
+ * 然而并没有抽象方法，具体方法在其各子类中，只是为了不让实例化，应该具体实例化其各个子类
+ * @author maxinchun
+ *
+ */
 public abstract class PushRequest {
 
 	@HttpParamKeyName(name=BaiduPushConstants.VERSION, param=R.OPTIONAL)
