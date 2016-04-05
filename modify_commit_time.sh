ff422ede82f2d9d967546f4b7a3902461703dbf2
codecly259 <maxinchun0215@qq.com>
2016-04-05 23:24:39 +0800

    just for updating

--- modify_commit_time.sh
+++ modify_commit_time.sh
@@ -1,8 +1,8 @@
 # clear previous backup
 rm -rf "$(git rev-parse --git-dir)/refs/original/"
 
-commit="3b97de6f23ce87716715710587c735016697d0fd"
-date=$(date -d 'a day ago')
+commit="1b99699ddc301e8180d9b25a0e3a99d21fff8897"
+date=$(date -d '1 day ago')
 git filter-branch --env-filter \
     "if [ \$GIT_COMMIT = '$commit' ]; then
          export GIT_AUTHOR_DATE='$date'
